e88e959f44b138066c077808c2b10372f748d146
saeechormunge23 <111693070+saeechormunge23@users.noreply.github.com>
2024-09-11 10:25:49 +0530

    Add files via upload

---
+++ Springxmlproject/SpringSecurityExample2/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class Controller {
+
+	@GetMapping("/delete")
+	public String delete() {
+		return "This is Delete Request only Admins can do this...";
+	}
+	
+	@GetMapping(value="/greeting")
+	public String greeting() {
+		return "This is Free Method anyone can access...";
+	}
+	
+	@GetMapping(value="/mcq")
+	public String mcq() {
+		return "This is MCQ Method only students can access...";
+	}
+	
+}
